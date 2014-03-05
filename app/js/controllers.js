167e3cd875d2d8b61dedaea98721b9b5159fdc8d
andosteinmetz <ando.steinmetz@gmail.com>
2014-03-05 16:47:38 -0500

    broke time formatter out into a service factory

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+  controller('MyCtrl1', ['$scope', '$http', 'dateFormatter', function($scope, $http, dateFormatter) {
         $scope.today = new Date();
         $http.get('shows/shows.json').success(function(data){
             $scope.shows = data;
@@ -11,8 +11,8 @@ angular.module('myApp.controllers', []).
             $scope.currentShows = [];
             $scope.upcomingShows = [];
             $scope.shows.forEach(function(show){
-                show.opening = new Date(show.openingDate);
-                show.closing = new Date(show.closingDate);
+                show.opening = dateFormatter(show.openingDate);
+                show.closing = dateFormatter(show.closingDate);
                 if(show.opening > $scope.today){
                    $scope.upcomingShows.push(show);
                 }
@@ -26,9 +26,11 @@ angular.module('myApp.controllers', []).
         });
   }])
 
-  .controller('ShowDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  .controller('ShowDetailCtrl', ['$scope', '$routeParams', '$http', 'dateFormatter', function($scope, $routeParams, $http, dateFormatter) {
     $scope.show = $routeParams.showId;
     $http.get('shows/' + $routeParams.showId + '.json').success(function(data){
         $scope.show = data;
+        $scope.show.opening = dateFormatter($scope.show.openingDate);
+        $scope.show.closing = dateFormatter($scope.show.closingDate);
     });
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,12 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+var myModule = angular.module('myApp');
+myModule.factory('dateFormatter', function(){
+    var dateFormatter = function(date){
+        var formattedDate = new Date(date);
+        return formattedDate;
+    }
+    return dateFormatter;
+});
