daf0f40444c97fb9d212609103857810fe68511e
andosteinmetz <ando.steinmetz@gmail.com>
2014-03-05 22:02:19 -0500

    going to override this  Please enter the commit message for your changes. Lines starting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,48 +3,33 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', function($scope) {
-        $scope.shows = [
-            {
-                id: 'rainbows',
-                title: 'Rainbows Have Nothing To Hide',
-                artists: [
-                    {name:'Roger Carmona'},
-                    {name:'Caitlin Keogh'},
-                    {name:'Marley Freeman'},
-                    {name:'Eric Legris'}
-                ],
-                release: 'Lorem ipsum dolor sit amet. Sator arepo tenet opera rotas',
-                works: ['one', 'two', 'three', 'four'],
-                openingDate: '07/28/13',
-                closingDate: '08/05/13'
-            },
-
-            {
-                id: 'about-time',
-                title: 'It\'s About Time',
-                artists: [
-                    {name:'Sarah Elliot'}
-                ],
-                release: 'Ipsum ipsum ipsum. Sator arepo tenet opera rotas',
-                works: ['table', 'object', 'thingy', 'display case'],
-                openingDate:'01/19/14',
-                closingDate:'01/27/14'
-            },
-
-            {
-                id: 'extra-bedroom',
-                title: 'Extra Bedroom',
-                artists: [
-                    {name:'Jojo Li'},
-                    {name:'Devon Costello'}
-                ],
-                release: 'Lorem lorem, ipsum ipsum, everything everything',
-                works: ['Bed', 'Table', 'Spill', 'Doodad'],
-                openingDate:'03/16/14',
-                closingDate:'04/03/14'
-            }
-        ];
+  controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+		$scope.today = new Date();
+		$scope.previousShows = new Array();
+     	$scope.upcomingShows = new Array();
+     	$scope.currentShows = new Array();
+		$http.get('shows/shows.json').success(function(data){
+			$scope.shows = data;
+			$scope.shows.forEach(function(show){
+				var openingDate = new Date(show.openingDate);
+				var closingDate = new Date(show.closingDate);
+				show.jsOpening = openingDate;
+				show.jsClosing = closingDate;
+				var today = $scope.today;
+				console.log($scope.upcomingShows);
+				if(openingDate > today){
+					$scope.upcomingShows.push(show);
+				}
+				else if(openingDate <= today){
+					if(closingDate >= today){
+						$scope.currentShows.push(show);
+					}
+					else{
+						$scope.previousShows.push(show);
+					}
+				}
+			});
+		});
   }])
   .controller('MyCtrl2', [function() {
 
