0a7ff007e27686a2cad3385a42c52a06f802d365
andosteinmetz <ando.steinmetz@gmail.com>
2014-06-03 12:55:56 -0400

    added tick marks, cleaned up clock

--- app/js/directives.js
+++ app/js/directives.js
@@ -44,29 +44,54 @@ angular.module('myApp.directives', []).
   .directive('mySVGCircle', function($interval){
     function link(scope, element, attrs){
 
+        var intervalId;
+
+        //time variables
+        var seconds;
+        var minutes;
+        var hours;
+
+        //clock variables
         var radius = 50;
         var strokeWidth = 4;
-
         var canvasSize = (radius *2)+(strokeWidth*2);
         var center = radius + strokeWidth;
 
+        //clock definitions
         var paper = Raphael(document.getElementById('circle'), canvasSize,canvasSize);
         var circle = paper.circle(center,center,radius);
-        var secondHand = paper.path("M"+center+" "+center+"L"+center+" "+strokeWidth);
-        var minuteHand = paper.path("M"+center+" "+center+"L"+center+" "+strokeWidth*2);
-        var hourHand = paper.path("M"+center+" "+center+"L"+center+" "+strokeWidth*4);
+        var secondHand = paper.path("M"+ center + " "+ center +"L"+ center +" "+ strokeWidth);
+        var minuteHand = paper.path("M"+ center + " " + center +"L"+ center +" "+ strokeWidth*2);
+        var hourHand = paper.path("M"+center+" "+ center +"L"+ center +" "+ strokeWidth*4);
+
+        function drawTickMark(deg){
+            var mark = paper.path("M"+ center +" "+ strokeWidth*2 +"L"+ center +" "+ strokeWidth*3);
+            mark.attr("stroke", "#666")
+            mark.transform("r"+ deg +","+ center +","+center);
+        }
+
+        function tickMarks(){
+            for(var i = 0; i < 12; i++){
+                var deg = i * 30;
+                drawTickMark(deg);
+            }
+        }
+
+        tickMarks();
 
         secondHand.attr('stroke', '#f00');
-
         circle.attr('fill', '#fff');
         circle.attr('stroke', '#000');
         circle.attr('sroke-width', strokeWidth);
 
         function updateClock(){
             var d = new Date();
-            var seconds = d.getSeconds();
-            var minutes = d.getMinutes();
-            var hours = d.getHours();
+            seconds = d.getSeconds();
+            minutes = d.getMinutes();
+            hours = d.getHours();
+
+            minutes = minutes + seconds/60;
+            hours = hours + minutes/60;
             if (hours > 12){
                 hours = hours - 12;
             }
@@ -75,14 +100,25 @@ angular.module('myApp.directives', []).
             var minuteHandRotation = (minutes/60) * 360;
             var hourHandRotation = (hours/12) * 360;
 
-            secondHand.transform("r"+secondHandRotation+","+center+","+center);
-            minuteHand.transform("r"+minuteHandRotation+","+center+","+center);
-            hourHand.transform("r"+hourHandRotation+","+center+","+center);
+            var nextSecond = (( seconds +1)/60) * 360;
+
+            secondHand.transform("r"+ secondHandRotation +","+center+","+ center);
+
+            //move second hand continuously
+            //secondHand.animate({transform: "r"+nextSecond+","+center+","+center}, 1000);
+            minuteHand.transform("r"+ minuteHandRotation +","+ center +","+ center);
+            hourHand.transform("r"+ hourHandRotation +","+ center +","+ center);
         }
 
-        $interval(function(){
+        updateClock();
+
+        intervalId = $interval(function(){
             updateClock();
         }, 1000);
+
+        element.bind('$destroy', function(){
+            $interval.cancel(intervalId);
+        });
 
     }
     return {
